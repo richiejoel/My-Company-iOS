3dcd91d0c85698e22880049da967e160e47bcdf0
joel garcia <juelgarcia98>
2020-02-25 16:47:23 -0500

    Initial Commit

---
+++ BusinessApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BusinessApplication
+//
+//  Created by Joel García on 2/25/20.
+//  Copyright © 2020 Joel García. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
