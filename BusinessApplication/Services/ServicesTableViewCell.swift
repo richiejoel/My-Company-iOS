9f85a9e352b82c8493e188b3146651a63cdf9ca5
Richard Joel Garcia <34136964+richiejoel@users.noreply.github.com>
2021-10-17 20:23:58 -0500

    Initial Porject 2020 🚀

---
+++ BusinessApplication/Services/ServicesTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  ServicesTableViewCell.swift
+//  BusinessApplication
+//
+//  Created by Joel García on 2/26/20.
+//  Copyright © 2020 Joel García. All rights reserved.
+//
+
+import UIKit
+
+class ServicesTableViewCell: UITableViewCell {
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- BusinessApplication/ViewController.swift
+++ BusinessApplication/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var btnServices: UIButton!
+    @IBOutlet weak var btnPortfolio: UIButton!
+    @IBOutlet weak var btnAboutUs: UIButton!
+    @IBOutlet weak var btnContactUs: UIButton!
+    @IBOutlet weak var btnSocialLinks: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        btnServices.layer.cornerRadius = 5
+        btnPortfolio.layer.cornerRadius = 5
+        btnAboutUs.layer.cornerRadius = 5
+        btnContactUs.layer.cornerRadius = 5
+        btnSocialLinks.layer.cornerRadius = 5
     }
-
-
+    
+    
+    @IBAction func mServices(_ sender: Any) {
+        self.tabBarController?.selectedIndex = 2
+    }
+    
+    @IBAction func mPortfolio(_ sender: Any) {
+        self.tabBarController?.selectedIndex = 3
+    }
+    
+    @IBAction func mAboutUs(_ sender: Any) {
+        self.tabBarController?.selectedIndex = 1
+    }
+    
+    @IBAction func mContactUs(_ sender: Any) {
+        self.tabBarController?.selectedIndex = 4
+    }
+    
+    
 }
 
